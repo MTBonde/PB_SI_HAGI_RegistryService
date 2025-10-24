6ed1bf9adcbf18183f0c195d3e8e1483bebca46c
MTBonde <thorbonde@gmail.com>
2025-10-24 15:33:17 +0200

    Update API version to 0.2.1

--- RegistryService/ApiVersion.cs
+++ RegistryService/ApiVersion.cs
@@ -2,7 +2,7 @@ namespace RegistryService;
 
 public static class ApiVersion
 {
-    public const string Current = "0.2.0";
+    public const string Current = "0.2.1";
     public const string Route = "v" + Current;
 }
 
